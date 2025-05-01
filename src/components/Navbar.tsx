ff2a26a10451f0f054c3a12e811b0725431b5f60
wirawanmahardika <wiraprogrammer@gmail.com>
2025-05-01 17:01:53 +0800

    feat: menambah confirmation modal sebelum logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,12 @@ import { NavLink, useMatch, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
     const navigate = useNavigate()
+
+    const openLogoutModal = () => {
+        const modal: any = document.getElementById('my_modal_5')
+        if (modal) { modal.showModal() }
+    }
+
     const logout = () => {
         localStorage.removeItem('token')
         navigate('/login')
@@ -12,9 +18,21 @@ export default function Navbar() {
     return <div className="dock dock-xl bg-neutral text-neutral-content">
         <Nav to="/" icon={<House />} title="Home" />
         <Nav to="/learn" icon={<BookOpen />} title="Learn" />
-        <button onClick={logout}>
+        <button onClick={openLogoutModal}>
             <LogOut />
             <span className="dock-label">Logout</span>
+
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Yakin ingin logout?</h3>
+                    <div className="gap-x-5 mt-5 flex justify-center">
+                        <form method="dialog">
+                            <button className="btn btn-error">Tidak</button>
+                        </form>
+                        <button onClick={logout} className="btn btn-success">Ya</button>
+                    </div>
+                </div>
+            </dialog>
         </button>
     </div>
 }
